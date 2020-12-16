546b7ff047d54b5220a66c7ca3cfde6f25e46a7a
sezaite <martyna.sezaite@gmail.com>
2020-12-16 20:40:05 +0000

    education constructor

--- js/components/education/Education.js
+++ js/components/education/Education.js
@@ -1,6 +1,13 @@
 class Education {
     constructor(params) {
+        this.selectorLeft = params.selectorLeft || 'body';
+        this.selectorRight = params.selectorRight || 'body';
+        this.data = params.data || [];
 
+        this.DOMleft = null;
+        this.DOMright = null;
+
+        this.init();
     }
 }
 
