12ee05d86cc2ed06966b1bbe2f525bbfc9600800
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-12-17 21:27:31 +0200

    Render services logic

--- js/components/services/renderServices.js
+++ js/components/services/renderServices.js
@@ -2,7 +2,22 @@
  * Our Services sekcija generuojanti funkcija
  * @param {string} selector  Css like salyga, kaip rasti norima vieta turinio generavimui
  * @param {Array} data  Sarasas objeku aprasanciu kiekviena Premium Feature
+ * @returns{*}
  */
 function renderServices(selector, data) {
+    // input valitation
 
+    // logic
+    const DOM = document.querySelector(selector);
+    
+    let HTML = '';
+    for (let service of data) {
+        console.log(feature);
+    }
+
+    DOM.innerHTML = HTML;
+
+    // post logic validation
+
+    // retur 
 }
