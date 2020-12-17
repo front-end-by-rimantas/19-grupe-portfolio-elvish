d40c61194b9b26947aacc12c5f229b3af843b300
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-12-17 21:19:22 +0200

    render services js files

---
+++ js/components/services/renderServices.js
@@ -0,0 +1,8 @@
+/**
+ * Our Services sekcija generuojanti funkcija
+ * @param {string} selector  Css like salyga, kaip rasti norima vieta turinio generavimui
+ * @param {Array} data  Sarasas objeku aprasanciu kiekviena Premium Feature
+ */
+function renderServices(selector, data) {
+
+}
