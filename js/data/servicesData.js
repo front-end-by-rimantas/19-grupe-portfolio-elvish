008460454bc76966d6dd582ddd6b3eab6e1af0ec
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-12-18 10:22:31 +0200

    changes

--- js/data/servicesData.js
+++ js/data/servicesData.js
@@ -41,6 +41,44 @@ const servicesData = {
   ] 
 }
 
+// const servicesData = [
+// {
+//   icon: 'mbri-database',
+//   title: 'Graphic Design',
+//   description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium provident quas expedita quibusdam cum eligendi.',
+// },
+// {
+//     icon: 'mbri-website-theme',
+//     title: 'Unlimited Color',
+//     description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium provident quas expedita quibusdam cum eligendi.',
+//   },
+//   {
+//     icon: 'mbri-growing-chart',
+//     title: 'Media Marketing',
+//     description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium provident quas expedita quibusdam cum eligendi.',
+//   },
+
+//   {
+//     icon: 'mbri-wifi',
+//     title: 'Unlimited Wifi',
+//     description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium provident quas expedita quibusdam cum eligendi.',
+
+//   },
+//   {
+//     icon: 'mbri-responsive',
+//     title: 'Responsive Design',
+//     description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium provident quas expedita quibusdam cum eligendi.',
+
+//   },
+//   {
+//     icon: 'mbri-edit',
+//     title: 'Easy to customize',
+//     description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium provident quas expedita quibusdam cum eligendi.',
+
+//   },
+       
+//   ] 
+
 
 
 export { servicesData }
