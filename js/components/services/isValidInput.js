4f5d289e942e4d635f635e3097ccae1ab33f2b2d
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-12-19 20:13:30 +0200

    additional input validation added

--- js/components/services/isValidInput.js
+++ js/components/services/isValidInput.js
@@ -1,13 +1,22 @@
 function isValidInput (selector, servData ) {
 
     if (typeof selector !== 'string') {
-        console.error('Error: premum features selector has to be a string type');
+        console.error('Error: our services selector has to be a string type');
         return false;
     }
     if (!Array.isArray(servData.data)) {
-        console.error('Error: premum features data has to be a array type');
+        console.error('Error: our services data has to be a array type');
         return false;
     }
+    if (selector === '') {
+        console.error('Error: our services data cannot be emty');
+        return false;
+    }
+    if (selector === 'number') {
+        console.error('Error: our services data cannot be numeric');
+        return false;
+    }
+
     return true;
 
 }
