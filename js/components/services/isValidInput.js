a8b29f778e67a428c26cfbf6685ba19f5be41b15
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-12-20 21:29:00 +0200

    input test added #109

--- js/components/services/isValidInput.js
+++ js/components/services/isValidInput.js
@@ -1,21 +1,32 @@
 function isValidInput (selector, servData ) {
 
     if (typeof selector !== 'string') {
-        console.error('Error: our services selector has to be a string type');
+        // console.error('Error: our services selector has to be a string type');
         return false;
     }
+        if (selector === '') {
+            // console.error('Error: our services data cannot be emty');
+            return false;
+        }
+        if (selector === 'number') {
+            // console.error('Error: our services data cannot be numeric');
+            return false;
+        }
+
+        if (typeof servData !== 'object' ||
+        Array.isArray(servData) ||
+        servData === null){
+        // console.error('Error: our services data has to be a array type');
+        return false;
+
+        }
+    
     if (!Array.isArray(servData.data)) {
-        console.error('Error: our services data has to be a array type');
+        // console.error('Error: our services data has to be a array type');
         return false;
     }
-    if (selector === '') {
-        console.error('Error: our services data cannot be emty');
-        return false;
-    }
-    if (selector === 'number') {
-        console.error('Error: our services data cannot be numeric');
-        return false;
-    }
+   
+    
 
     return true;
 
