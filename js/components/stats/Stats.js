48d2c2aadf30f49c959c406982fff773fa646f5b
sezaite <martyna.sezaite@gmail.com>
2020-12-23 08:29:25 +0000

    Update Stats.js

--- js/components/stats/Stats.js
+++ js/components/stats/Stats.js
@@ -63,7 +63,7 @@ class Stats {
             }
             HTML += `<div class="counter col-3 col-sm-12">
                         <i class="mbri-${item.icon}" aria-hidden="true"></i>
-                        <h2>${item.header}</h2>
+                        <h2>0</h2>
                         <p>${item.text}</p>
                     </div>`;
         }
@@ -88,39 +88,44 @@ class Stats {
     }
 
     counterAnimation(counterIndex) {
-        let count = 0;
-        let tick = 0;
+        const finalNumber = this.data[counterIndex].header;
         const totalTickCount = this.animationDuration * this.animationFPS;
         const numberDOM = this.countersDOMs[counterIndex].querySelector('h2');
+        let count = 0;
+        let tick = 0;
+
         const timer = setInterval(() => {
-            count = Math.floor(tick / totalTickCount * this.data[counterIndex].header);
+            count = Math.floor(tick / totalTickCount * finalNumber);
             tick++;
             numberDOM.innerText = count;
-            if (this.data[counterIndex].header === count) {
+            if (finalNumber === count) {
                 clearInterval(timer);
             }
         }, 1000 / this.animationFPS)
     }
 
-    addEvents() {
-        addEventListener('scroll', () => {
-            const windowBottom = scrollY + innerHeight;
-            let counterBottom = 0;
-            for (let i = 0; i < this.countersDOMs.length; i++) {
-                if (this.countersDOMs[i].animated) {
-                    continue;
-                }
-                const counter = this.countersDOMs[i];
-                let counterPosition = counter.getBoundingClientRect();
-                counterBottom = counterPosition.bottom + scrollY;
+    shouldCounterAnimationRun() {
+        const windowBottom = scrollY + innerHeight;
+        let counterBottom = 0;
+        for (let i = 0; i < this.countersDOMs.length; i++) {
+            if (this.data[i].animated) {
+                continue;
+            }
+            const counter = this.countersDOMs[i];
+            let counterPosition = counter.getBoundingClientRect();
+            counterBottom = counterPosition.bottom + scrollY;
 
-                if (counterBottom < windowBottom) {
-                    this.data[i].animated = true;
-                    this.counterAnimation(i);
-                }
+            if (counterBottom < windowBottom) {
+                this.data[i].animated = true;
+                this.counterAnimation(i);
             }
         }
-        )
+    }
+
+    addEvents() {
+        addEventListener('scroll', () => {
+            this.shouldCounterAnimationRun();
+        });
     }
 
     isCounterValid() {
